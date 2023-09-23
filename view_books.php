fa43596d237085a5a8dc44782620362a0b57662f
Mochammad Arya Jadmika <100180173+MochammadAryaJadmika@users.noreply.github.com>
2023-09-24 00:51:19 +0700

    Update view_books2.0.php

--- view_books.php
+++ view_books.php
@@ -1,17 +1,14 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['username'])) {
-        header('Location: login.php');
-    }
-
     include('./header.php') 
 ?>
-
     <div class="card mt-5">
         <div class="card-header">Books Data</div>
         <div class="card-body">
-            <a href="add_books.php" class="btn btn-primary mb-4">+ Add Books Data</a>
-            <a href="logout.php" class="btn btn-danger mb-4">Logout</a>
+            <a href="add_books.php" class="btn btn-primary mb-4"><i class="bi bi-plus-circle-fill"></i>&nbsp; Add Books Data</a>
+            <a href="search_books.php" class="btn btn-primary mb-4"><i class="bi bi-search"></i>&nbsp; Search Books Data</a>
+            <a href="order_books.php" class="btn btn-primary mb-4"><i class="bi bi-cart4"></i>&nbsp; Order Books</a>
+            <a href="data_rekap.php" class="btn btn-primary mb-4"> <i class="bi bi-bar-chart-fill"></i>&nbsp;Book Recap</a>
+            <a href="books_category.php" class="btn btn-primary mb-4"> <i class="bi bi-journals"></i>&nbsp;Book Category</a>
             <br>
             <table class="table table-striped">
                 <tr>
@@ -42,18 +39,21 @@
                     echo '<td>' . $row->name . '</td>';
                     echo '<td>' . $row->author . '</td>';
                     echo '<td>' . $row->price . '</td>';
-                    echo '<td><a class="btn btn-warning btn-sm" href="edit_books.php?id=' . $row->isbn . '">Edit</a>&nbsp;
-                    <a class="btn btn-danger btn-sm" href="delete_books.php?id=' . $row->isbn . '">Delete</a></td>';
+                    echo '<td><a class="btn btn-warning btn-sm" href="edit_books.php?id=' . $row->isbn . '"><i class="bi bi-pencil-square"></i>&nbsp; Edit</a>&nbsp;
+                    <a class="btn btn-danger btn-sm" href="delete_books.php?id=' . $row->isbn . '"><i class="bi bi-trash"></i>&nbsp; Delete</a></td>';
                     echo '</tr>';
                 }
       
                 echo '</table>';
-                echo '<br />';
                 echo 'Total Rows = ' . $result->num_rows;
-
+                echo '<br />';
+                
                 $result->free();
                 $db->close();
                 ?>
+
+            <br>
+            <a href="logout.php" class="btn btn-dark mb-4"> Logout</a>
         </div>
     </div>
 
